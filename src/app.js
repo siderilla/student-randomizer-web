84c3fd64048feec2e4701f71492b18aff4dca51e
siderilla <siderilla11@gmail.com>
2025-03-12 12:42:36 +0100

    menu bottoni random

--- src/app.js
+++ src/app.js
@@ -2,57 +2,96 @@ import dataService from "./services/data-service.js";
 
 const service = new dataService();
 
+function orderByName() {
+    const studentData = service.getStudentsByName();
+    render(studentData);
+}
+
+function orderByAge() {
+    const studentData = service.getStudentsByAge();
+    render(studentData);
+}
+
+function shuffle() {
+    const studentData = service.getShuffledStudents();
+    render(studentData);
+}
+
+window.orderByName = orderByName;
+window.orderByAge = orderByAge;
+window.shuffle = shuffle;
+
 // const studentData = service.getStudentsByAge();
 // const studentData = service.getStudentsByName();
-const studentData = service.getShuffledStudents();
-
+// const studentData = service.getShuffledStudents();
 
 // funzione sort per ordinare alfabeticamente
 // studentData.sort((a, b) => a.name.localeCompare(b.name));
 
-const container = document.getElementById('students-container');
+function render(studentData) {
+    const container = document.getElementById('students-container');
 
-for (let i = 0; i < studentData.length; i++) {
-    const student = studentData[i];
+    container.innerHTML = '';
 
-    const studentContainer = document.createElement('div');
-    if(i%2 === 0){
-        studentContainer.classList.add('student-container');
-    } else {
-        studentContainer.classList.add('student-container2');
-    }
-
-    const nameContainer = document.createElement('span');
-    nameContainer.classList.add('name-container');
-    const nameNode = document.createTextNode(student.name + ' ' + student.surname);
-    nameContainer.appendChild(nameNode);
-
-    const countryContainer = document.createElement('span');
-    countryContainer.classList.add('country-container');
-    const countryNode = document.createTextNode('Nazionalità: ' + student.nationality);
-    countryContainer.appendChild(countryNode);
-
-    const genderContainer = document.createElement('span');
-    genderContainer.classList.add('gender-container');
-    const genderNode = document.createTextNode('Gender: ' + student.gender);
-    genderContainer.appendChild(genderNode);
-
-    // const now = new Date();
-    // const year = now.getFullYear();
-    // const age = year - student.yob;
-    // const ageNode = document.createTextNode('Age: ' + age);
+    for (let i = 0; i < studentData.length; i++) {
+        const student = studentData[i];
     
-    const ageContainer = document.createElement('span');
-    ageContainer.classList.add('age-container');
-    const ageNode = document.createTextNode('Age: ' + student.getAge());
-    ageContainer.appendChild(ageNode);
+        const studentContainer = document.createElement('div');
+        if(i % 2 === 0){
+            studentContainer.classList.add('student-container');
+        } else {
+            studentContainer.classList.add('student-container2');
+        }
+        
+        const nameContainer = createTextElement('span', student.name + ' ' + student.surname);
 
-    studentContainer.appendChild(nameContainer);
-    studentContainer.appendChild(countryContainer);
-    studentContainer.appendChild(genderContainer);
-    studentContainer.appendChild(ageContainer);
+        // const nameContainer = document.createElement('span');
+        // nameContainer.classList.add('name-container');
+        // const nameNode = document.createTextNode(student.name + ' ' + student.surname);
+        // nameContainer.appendChild(nameNode);
 
-    container.appendChild(studentContainer);
+        const countryContainer = createTextElement('span', 'Nazionalità: ' + student.nationality);
+    
+        // const countryContainer = document.createElement('span');
+        // countryContainer.classList.add('country-container');
+        // const countryNode = document.createTextNode('Nazionalità: ' + student.nationality);
+        // countryContainer.appendChild(countryNode);
+
+        const genderContainer = createTextElement('span', 'Gender: ' + student.gender);
+
+        // const genderContainer = document.createElement('span');
+        // genderContainer.classList.add('gender-container');
+        // const genderNode = document.createTextNode('Gender: ' + student.gender);
+        // genderContainer.appendChild(genderNode);
+
+        const ageContainer = createTextElement('span', 'Age: ' + student.getAge());
+    
+        // const now = new Date();
+        // const year = now.getFullYear();
+        // const age = year - student.yob;
+        // const ageNode = document.createTextNode('Age: ' + age);
+        
+        // const ageContainer = document.createElement('span');
+        // ageContainer.classList.add('age-container');
+        // const ageNode = document.createTextNode('Age: ' + student.getAge());
+        // ageContainer.appendChild(ageNode);
+    
+        studentContainer.appendChild(nameContainer);
+        studentContainer.appendChild(countryContainer);
+        studentContainer.appendChild(genderContainer);
+        studentContainer.appendChild(ageContainer);
+    
+        container.appendChild(studentContainer);
+    }
+}
+
+function createTextElement(elementType, text) {
+
+    const element = document.createElement(elementType);
+    const node = document.createTextNode(text);
+    element.appendChild(node);
+    return element;
+
 }
 
 //// random background color palette
